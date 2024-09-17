86b61717a947ba5fd83f42d5239ae915cda3fedd
sonalichaudhary844@gmail.com <sonalichaudhary884>
2024-09-18 02:12:52 +0530

    fragment

--- src/App.jsx
+++ src/App.jsx
@@ -16,49 +16,47 @@ function App() {
   console.log("APP COMPONENT EXECUTING");
 
   return (
-    <div>
-      <Header />
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept {...conceptItem} />
-            ))}
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
-              {" "}
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX </TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State </TabButton>
-          </menu>
-          {!selectedTopic && <p>Please select a topic.</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
-      </main>
-    </div>
+    <>
+      <div>
+        <Header />
+        <main>
+          <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+              {CORE_CONCEPTS.map((conceptItem) => (
+                <CoreConcept {...conceptItem} />
+              ))}
+            </ul>
+          </section>
+          <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+              <TabButton onSelect={() => handleSelect("components")}>
+                {" "}
+                Components
+              </TabButton>
+              <TabButton onSelect={() => handleSelect("jsx")}>JSX </TabButton>
+              <TabButton onSelect={() => handleSelect("props")}>
+                Props
+              </TabButton>
+              <TabButton onSelect={() => handleSelect("state")}>
+                State{" "}
+              </TabButton>
+            </menu>
+            {!selectedTopic && <p>Please select a topic.</p>}
+            {selectedTopic && (
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+              </div>
+            )}
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
 
